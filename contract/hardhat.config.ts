9409f2eec246968fbda84a95b9404795f03b2228
Angelo Verlain <hey@vixalien.com>
2026-07-03 13:57:02 +0200

    fix: enable optimizer in hardhat config to fit contract bytecode, update ABIs

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -5,18 +5,11 @@ import "dotenv/config";
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
