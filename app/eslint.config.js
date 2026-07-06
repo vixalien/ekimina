c2ffdc7f6ee375589c9b8548b189bd03fbf99939
Angelo Verlain <hey@vixalien.com>
2026-07-06 20:01:28 +0200

    chore: fmt and lint

--- app/eslint.config.js
+++
@@ -1,8 +0,0 @@
-const expoConfig = require("eslint-config-expo/flat");
-
-module.exports = [
-  ...expoConfig,
-  {
-    ignores: ["dist/*", ".expo/*", "node_modules/*", "expo-env.d.ts", "uniwind-types.d.ts"],
-  },
-];
